9dea1c8134da0d495f56365e9fe1aeeb952df7ab
Arvid A <arvid.anderson@outlook.com>
2020-08-26 17:11:23 +0200

    Create if-elif-else.py

---
+++ Python/Learning/if-elif-else.py
@@ -0,0 +1,14 @@
+
+
+#Variabel, byt denna för att se resultat
+
+testet = (1)
+ 
+#if else och else
+
+if testet == (1):
+    print("Variablen testet hade värdet av 1")
+elif testet == (2):
+    print("Variablen testet hade värdet av 2")
+else:
+    print("Variablen hade värdet utav något annat än 1 eller 2")
