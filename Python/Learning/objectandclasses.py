d1451058b0ba6793954e88add3c518a61b1c4566
Arvid Anderson <arvid.anderson@elev.ga.ntig.se>
2020-09-03 10:25:11 +0200

    sad

--- Python/Learning/objectandclasses.py
+++ Python/Learning/objectandclasses.py
@@ -2,22 +2,38 @@
 
 #Gör klassen Robot
 
+from os import system
+from os import name
+
+
+if name == 'nt':
+    system('cls')
+else:
+        system('clear')
+
+
+spar1 = open("objectsparfil.txt", "+w")
+
 class Robot:
     def introduce_self(self):
-        print("My name is " + self.name)
+        return("My name is " + self.name)
 
-#
 r1 = Robot()
 r1.name = "Kazung"
 r1.color = "Blue"
 r1.weight = 70
 
+
 #
 r2 = Robot()
-r2.name = "Boliver"
+r2.name = "lol"
 r2.color = "Pink"
 r2.weight = 25
 
+
 #Kör funktionen introuduce self
-r1.introduce_self()
-r2.introduce_self()
+spar1.write(r1.introduce_self())
+spar1.write("\n")
+spar1.write(r2.introduce_self())
+
+
