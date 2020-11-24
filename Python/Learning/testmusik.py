0d30a26139d577970a49b9b88ce03de5f4338d8a
Arvid A <arvid.anderson@outlook.com>
2020-11-24 20:32:44 +0100

    Spela upp ljudfil test

---
+++ Python/Learning/testmusik.py
@@ -0,0 +1,17 @@
+#Musik i python test
+#2020 24 NOV
+from playsound import playsound
+
+
+#Definerar funktionen
+def ljud():
+    #Variabel med innehållet av min ljudfil som ligger i samma mapp och heter ljudtest.mp3
+    minljudfil = "ljudtest.mp3"
+    #Använder playsound modulen för att spela upp filen
+    playsound(minljudfil)
+
+
+
+#Kör funktionen
+ljud()
+
